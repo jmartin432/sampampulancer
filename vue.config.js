8aaad74b0c32e229d2a6e33f6e9ec3204647348b
Justin <info@justinlmartin.com>
2020-08-11 17:15:13 -0700

    updating configs and package.json

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
+    plugins: [
+      new htmlWebpackPlugin({  // Also generate a test.html
+        title: 'Sampampulancer'
+      })
+    ],
     module: {
       rules: [
         {
